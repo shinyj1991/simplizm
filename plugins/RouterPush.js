c2fb177ba88823d3069d9df486256c4ac3c73317
simplizm-company <40747170+simplizm-company@users.noreply.github.com>
2020-08-14 16:53:56 +0900

    before

--- plugins/RouterPush.js
+++ plugins/RouterPush.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 
 export default function(context) {
-  context.app.router.afterEach(function(to) {
+  context.app.router.beforeEach(function(to) {
     context.app.router.push(to.fullPath);
   });
 }
